916a2358cb2eabaf1ef098549c4760de53759c2f
𝐒𝐭𝐚𝐫𝐋𝐲𝐧 এ <danoninoleonel@gmail.com>
2026-06-04 08:50:30 -0600

    Crear config.js

---
+++ config.js
@@ -0,0 +1,11 @@
+export default {
+    nombre: 'Roxy',
+    nombre2: 'Migurdia',
+    prefix: '.',
+    owner: ['5219992042946'],
+    version: '1.0.0',
+    tipo: 'principal',
+    creador: 'ぱ𝒮𝓉𝒶𝓇𝐿𝓎𝓃꒱᮫🌙〪̥〫᪲⸽⁞',
+    canalId: '120363427517609839@newsletter',
+    canalNombre: '  ⃞̶〪〫᪱ᩞ᮫ֹᦡᰱ᮫〭 ͜͝ 𝑳𝒊𝒈𝒉𝒕 𝑺𝒕𝒂𝒓◞ 𓈒 ⿻⭐⃜⡻'
+};
